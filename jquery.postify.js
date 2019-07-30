a4adebedbe7f34ff9f9f5e03f3a35b83ad8e357d
Marius Butz Mobile <mbpictures13@gmail.com>
2019-07-30 13:15:08 +0200

    Post Load elements with adjust height of parent + demo

--- jquery.postify.js
+++ jquery.postify.js
@@ -3,7 +3,7 @@
     $.fn.postify = function(options) {
 		
 	 
-		var settings = $.extend( {}, defaults, options );
+		var settings = $.extend( {}, $.fn.postify.defaults, options );
 		
 		//initialize with loading
 		this.each(function () {
@@ -14,21 +14,30 @@
 	 
 		return this.each(function() {
 			var elem = $(this);
-			elem.css("opacity", "0");
-			
+			var height = $(this).parent().css("height");
+			//elem.css("opacity", "0");
 			if(elem.data("postify")){
+				var src = elem.data("postify");
 				var newImg = $("<img>");
 				newImg.load(function () {
-					elem.attr("src", $(this).attr("postify"));
 					
-					elem.find(loadingClass, function(){
-						$(this).remove();
+					elem.find('.' + settings.loadingClass).each(function(){
+						$(this).delay(settings.delay).fadeOut(settings.duration);
+						
 					});
+					newImg.css({
+						position: "absolute",
+						top: "0",
+						left: "0",
+						width: "100%"
+					}).hide().delay(settings.delay).appendTo(elem).fadeIn(1000);
 					
-					elem.
+					if(settings.adjustParentSize == "height"){
+						elem.parent().delay(settings.delay).animate({height: newImg.css("height")}, settings.duration);
+					}
 				});
 				
-				newImg.attr("src", elem.data("postify"));
+				newImg.attr("src", src);
 				
 			}
 			else if(elem.data("postify-bg")){
@@ -40,7 +49,7 @@
 						$(this).remove();
 					});
 					
-					elem.
+					elem.fadeIn("slow");
 				});
 				
 				newImg.attr("src", elem.data("postify-bg"));
@@ -52,12 +61,14 @@
     };
 	
 	$.fn.postify.defaults = {
-		animation: "fade",
-		easing: "ease",
-		duration: "slow",
-		showLoading: true,
-		loadingClass: "postify-loading",
-		loadingElements: ""
+		"animation": "fade",
+		"easing": "ease",
+		"duration": "slow",
+		"delay": 0,
+		"showLoading": true,
+		"loadingClass": "postify-loading",
+		"loadingElements": "<div></div><div></div><div></div><div></div>",
+		"adjustParentSize": "height"
 	};
  
 }( jQuery ));
